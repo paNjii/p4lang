8e2b6157fe944ef0bff52f0727b9267aca8f5f53
Jeffrey Panneman <jpan@live.nl>
2018-10-13 22:52:19 +0200

    -Resized icon a bit -Now loads language support

--- src/nl/p4lang/P4FileTypeFactory.java
+++ src/nl/p4lang/P4FileTypeFactory.java
@@ -3,7 +3,7 @@ package nl.p4lang;
 import com.intellij.openapi.fileTypes.*;
 import org.jetbrains.annotations.NotNull;
 
-public class P4ileTypeFactory extends FileTypeFactory {
+public class P4FileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
         fileTypeConsumer.consume(P4FileType.INSTANCE);
